ef36705b4d143e8fc0a46da4a82338794c476f58
shubrautt <shubham162001@gmail.com>
2024-08-18 14:41:07 +0530

    feat: toaster moved to App

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,22 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import ProtectedRoutes from './utils/ProtectedRoutes';
 import LoginPage from './pages/LoginPage';
 import SignUpPage from './pages/SignUpPage';
+import ToasterProvider from './contexts/ToasterContext';
 
 function App() {
   return (
     <div className='container'>
-      <BrowserRouter>
-        <Routes>
-          <Route element={<ProtectedRoutes />}>
-            <Route element={<></>} path='/' />
-          </Route>
-          <Route element={<SignUpPage />} path='/signup' />
-          <Route element={<LoginPage />} path='/login' />
-        </Routes>
-      </BrowserRouter>
+      <ToasterProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route element={<ProtectedRoutes />}>
+              <Route element={<></>} path='/' />
+            </Route>
+            <Route element={<SignUpPage />} path='/signup' />
+            <Route element={<LoginPage />} path='/login' />
+          </Routes>
+        </BrowserRouter>
+      </ToasterProvider>
     </div>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,10 @@ import './global.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import ToasterProvider from './contexts/ToasterContext.tsx';
 import App from './App.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ToasterProvider>
-      <App />
-    </ToasterProvider>
+    <App />
   </React.StrictMode>
 );
